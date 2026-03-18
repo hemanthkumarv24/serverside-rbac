79666547e4492651d5392f4320de38152c7ad1e0
Hemanth Kumar V <babubangaram688@gmail.com>
2026-03-18 14:15:12 +0530

    fix: set nextauth route to nodejs runtime

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
-import { handlers } from '@/lib/auth'
-
-export const { GET, POST } = handlers
+import { handlers } from "@/lib/auth";
+export const runtime = "nodejs";
+export const { GET, POST } = handlers;
